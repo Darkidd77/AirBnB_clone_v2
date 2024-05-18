7d95e1e8ef038abf4b3d8b66798e9c5e0b980cb8
darkidd77 <m.almahdi77@yahoo.com>
2024-05-18 16:12:31 +0100

    task-6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,7 +3,7 @@
 start Flask app
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -30,6 +30,29 @@ def cisfun(text):
 def pythoniscool(text='is cool'):
     """display “Python ”, followed by value of text variable"""
     return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def imanumber(n):
+    """display “n is number”  if n is integer"""
+    return "{:d} is number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def numbersandtemplates(n):
+    """display  HTML page if n is integer"""
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def numbersandevenness(n):
+    """display HTML page only if n is integer"""
+    if n % 2 == 0:
+        evenness = 'even'
+    else:
+        evenness = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n,
+                           evenness=evenness)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
